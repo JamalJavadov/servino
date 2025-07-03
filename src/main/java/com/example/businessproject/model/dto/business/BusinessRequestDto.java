0fd0641b8ddc79201c22dc478f3ee334ea72d64a
Jamal Javadov <camalcavadovwork@gmail.com>
2025-07-03 20:06:40 +0400

    Commit 1

--- src/main/java/com/example/businessproject/model/dto/business/BusinessRequestDto.java
+++ src/main/java/com/example/businessproject/model/dto/business/BusinessRequestDto.java
@@ -1,5 +1,8 @@
 package com.example.businessproject.model.dto.business;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -8,17 +11,33 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BusinessRequestDto {
+
+    @NotBlank(message = "Password Can not be empty")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?!.*[<>#%])[A-Za-z0-9!@\\$\\^\\&\\*\\(\\)_\\+\\-=\\{\\}\\[\\]:;\"'\\|\\\\,.\\?/]{8,15}$",
+            message = "Password must be 8-15 characters, include at least one digit, one uppercase letter, one lowercase letter, and not contain <, >, #, % symbols"
+    )
     private String password;
 
+    @NotBlank(message = "BusinessName Can not be empty")
+    @Pattern(
+            regexp = "^[^<>#%]{0,50}$",
+            message = "Text must be up to 50 characters and must not contain <, >, #, % symbols"
+    )
     private String businessName;
 
+    @NotBlank(message = "Location Can not be empty")
     private String businessLocation;
 
     private String description;
 
+    @Email(message = "Email Must be valid")
+    @NotBlank(message = "Email Can not be empty")
     private String contactMail;
 
+    @NotBlank(message = "Number can not be empty")
     private String contactNumber;
 
+    //burani secim olaraq etmek isteyirem
     private String category;
 }
--- src/main/java/com/example/businessproject/model/dto/business/BusinessUpdateDto.java
+++ src/main/java/com/example/businessproject/model/dto/business/BusinessUpdateDto.java
@@ -4,6 +4,9 @@ import com.example.businessproject.model.dto.product.ProductRequestDto;
 import com.example.businessproject.model.dto.product.ProductUpdateDto;
 import com.example.businessproject.model.entity.Product;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -16,16 +19,26 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BusinessUpdateDto {
+    @NotBlank(message = "BusinessName Can not be empty")
+    @Pattern(
+            regexp = "^[^<>#%]{0,50}$",
+            message = "Text must be up to 50 characters and must not contain <, >, #, % symbols"
+    )
     private String businessName;
 
+    @NotBlank(message = "Location Can not be empty")
     private String businessLocation;
 
     private String description;
 
+
     private String logoUrl;
 
+    @Email(message = "Email Must be valid")
+    @NotBlank(message = "Email Can not be empty")
     private String contactMail;
 
+    @NotBlank(message = "Number can not be empty")
     private String contactNumber;
 
     private String category;
